8c1e74ee352169f5b3c5f2458492c17407c2d7ae
Conanjacko <pau.millan@duocuc.cl>
2024-12-12 16:40:58 -0300

    ADMIN agrega productos y se reflejan en inicio. No se ven imagenes al inicio, arreglar eso

--- egesven/productos/views.py
+++ egesven/productos/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Producto
 
 def landing(request):
-    return render(request, 'inicio.html')
+    productos = Producto.objects.all()
+    return render(request, 'inicio.html', {'productos': productos})
+
+def productos(request):
+    productos = Producto.objects.all()  
+    return render(request, 'productos.html', {'productos': productos})
+
